12fa06b01bd702706b3439ebccb13606b352c513
NAME <a@a.com>
2022-02-07 13:24:38 +0700

    commit1

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<div class="root">
+	<slot />
+</div>
+
+<style>
+	.root {
+		position:absolute;
+		width: 100%;
+		height: 100%;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,53 @@
+<script>
+	import { LayerCake, Svg } from 'layercake';
+
+	import Line from './layercake/Line.svelte';
+	import Area from './layercake/Area.svelte';
+	import AxisX from './layercake/AxisX.svelte';
+	import AxisY from './layercake/AxisY.svelte';
+
+	const xKey = 'myX';
+	const yKey = 'myY';
+
+	let points = [
+		{
+			myX: 1979,
+			myY: 7.19
+		},
+		{
+			myX: 1980,
+			myY: 7.73
+		},
+		{
+			myX: 1981,
+			myY: 2.73
+		},
+		{
+			myX: 1982,
+			myY: 12.73
+		}
+	];
+</script>
+<div class="chart-container">
+	<LayerCake
+		padding={{ right: 10, bottom: 20, left: 25 }}
+		x={xKey}
+		y={yKey}
+		yDomain={[0, null]}
+		data={points}
+	>
+		<Svg>
+			<AxisX/>
+			<AxisY/>
+			<Line/>
+			<Area/>
+		</Svg>
+	</LayerCake>
+</div>
+
+<style>
+	.chart-container {
+		width: 100%;
+		height: 100%;
+	}
+</style>
